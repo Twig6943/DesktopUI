549a93a5bd7d721c616ad716aa13972aa8a44fd7
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-01-20 16:02:01 +0300

    Misc fixes
    - Rename logo to make it fit standards + update fpm config for the name change
    - Make CI just use installed stuff (binary,icon,desktop file) + use latest sharun + remove leftovers
    - Fix mistake in .gitignore
    - Replace the email of the previous maintainer with mine
    - Add Network to .desktop file
    - Add StartupWMClass to the .desktop file so sharun stops complaining

--- pkg/AppImage/AppImage.sh
+++ pkg/AppImage/AppImage.sh
@@ -6,17 +6,14 @@
 set -eu
 
 ARCH="$(uname -m)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/753b3ba3c77a573f8c2eeb0b397752df2d3235df/useful-tools/quick-sharun.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/main/useful-tools/quick-sharun.sh"
 
 export ADD_HOOKS="self-updater.bg.hook"
 #export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export OUTNAME=zerotier-desktop-ui-anylinux-"$ARCH".AppImage
-export DESKTOP=./usr/share/applications/zerotier-ui.desktop
-export ICON=./usr/share/icons/hicolor/512x512/apps/zerotier.png
+export DESKTOP=/usr/share/applications/zerotier-ui.desktop
+export ICON=/usr/share/icons/hicolor/512x512/apps/zerotier.png
 export OUTPATH=./dist
-export DEPLOY_OPENGL=0
-export DEPLOY_VULKAN=0
-export DEPLOY_DOTNET=0
 
 #Remove leftovers
 rm -rf AppDir dist appinfo
@@ -26,7 +23,7 @@ wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 
 # Point to binaries
-./quick-sharun ./usr/bin/zerotier_desktop_ui
+./quick-sharun /usr/bin/zerotier_desktop_ui
 
 # Make AppImage
 ./quick-sharun --make-appimage
